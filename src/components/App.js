d9a6e14ae8d8797549a32ed9f9c4d754935c9ff3
william huang <hylbegoat@gmail.com>
2018-09-13 17:22:49 +0800

    feature: add transform-object-rest-spread plugin to support spread operator in es6

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 export default class App extends Component {
   render () {
+    const test = {1: 2, 2: 3};
+    console.log({...test, 2: 4});
     return (
       <p>This is my new react app</p>
     );
